3b0f292eb29256048e3d06b5d47560dc8f9e3713
Thymck <thymucak@gmail.com>
2023-06-13 13:41:58 +0200

    update

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 document.getElementById("onMenu").addEventListener("click", function () {
   var menuItems = document.getElementById("menuItems");
+  var iconTimes = document.getElementById("onMenu");
   menuItems.classList.toggle("active");
+  iconTimes.classList.toggle("active");
 });
